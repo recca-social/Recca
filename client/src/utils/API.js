1c60945cba871af7aa55ebbb134911015ed41e27
Cadin McQueen <cadin.mcqueen@gmail.com>
2019-01-05 20:29:07 -0500

    Add axios and API starter

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,1 +1,26 @@
+import axios from "axios";
 
+const googleBooksURL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+// Export an object with a "search" method that searches the Google Books API for the passed query
+export default {
+  search: function(query) {
+    return axios.get(googleBooksURL + query);
+  },
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Gets the book with the given id
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  }
+};

3980ac6654b37f6328a5f9c962984efc69eae92d
Cadin McQueen <cadin.mcqueen@gmail.com>
2019-01-08 21:47:01 -0500

    Add inital sidebar/results components

---
+++ client/src/components/Results/bookResults.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./results.scss";
+
+function bookResults(props) {
+  return (
+    <div>
+
+    </div>
+
+    // <div className="results">
+    //   {props.books.map(book => (
+    //     <div className="list-group-item result" key={book.id}>
+    //       <div className="result__header">
+    //         <h3 className="result__title">{book.title}</h3>
+    //         <div className="result__buttons">
+    //           <a href={book.link} target="_blank" rel="noopener noreferrer" className="btn btn-primary view">View</a>
+    //           {props.currentPage === "search" ? 
+    //             <button onClick={() => props.handleBookSave(book.id)} className="btn btn-primary save">Save</button> :
+    //             <button onClick={() => props.handleDelete(book.id)} className="btn btn-danger save">Remove</button>
+    //           }
+                
+    //         </div>
+    //       </div>
+    //       <p className="result__byline">
+    //         Author(s): <strong>{book.authors}</strong>
+    //       </p>
+    //       <img
+    //         alt={book.title} className="results__img"
+    //         src={book.image} />
+    //       <p className="results__description">
+    //         {book.description}
+    //       </p>
+    //       <div className="clearfix"></div>
+    //     </div>
+    //   ))}
+    // </div>
+  );
+}
+
+export default bookResults;

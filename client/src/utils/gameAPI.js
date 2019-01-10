501a82c221d62714d202539f8c1ccc5be7381095
Eric Keyes <keyes.eric@outlook.comgit config --global user.email keyes.eric@outlook.com>
2019-01-10 10:18:04 -0500

    video game api completed, front end completed, spotify front end updated

--- client/src/utils/gameAPI.js
+++ client/src/utils/gameAPI.js
@@ -1,11 +1,7 @@
 import axios from "axios";
-const apiKey = "72783dc69b855c01f1c0618ddb9fed6f"
 
-// export default {
-
-//     search: function(query){
-//        axios({
-//            url: 
-//        })
-//     }
-// }
+export default {
+    search: function(query){
+        return axios.get("api/vg/search" + query)
+    }
+}

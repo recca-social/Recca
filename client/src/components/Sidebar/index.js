d6a818efa3cc43cfa79c46c92f983d30f46ef411
Cadin McQueen <cadin.mcqueen@gmail.com>
2019-01-11 14:59:16 -0500

    Add active button to sidebar items

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -11,7 +11,7 @@ function Sidebar(props) {
   }
   return (
     <div className="col-md-3 sidebar">
-      <div className="sidebar__title">Active Media <i className="icon icon-eye"></i></div>
+      <div className="sidebar__title">Active Media</div>
       <div className="sidebar__content">
         {/* Check for media items and check that there is at least 1 active item */}
         {props.items && props.items.filter(item => item.active === true).length >= 1 ? 
@@ -20,6 +20,9 @@ function Sidebar(props) {
             <img src={item.image} alt={item.title} className="sidebar__img" />
             <a href={'#' + item.apiId} className="sidebar__link"><strong>{item.title}</strong></a>
             <p>{displayMetadata(item.type, item.creator, item.platform)}</p>
+            <button onClick={() => props.toggleActive(item._id) } className="btn-active">
+              <i className="icon icon-eye"><span className="sr-only">Set inactive</span></i>
+            </button>
             <div className="clearfix"></div>
           </div>
         )) : <p className="text-center">Start setting your active media</p> }

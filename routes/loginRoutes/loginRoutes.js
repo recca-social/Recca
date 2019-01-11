e464a3eb78857c298d10fd6768de473d17026983
H. B. Young <HarryBeckerYoung@gmail.com>
2019-01-10 19:07:09 -0500

    more small fixes

--- routes/loginRoutes/loginRoutes.js
+++ routes/loginRoutes/loginRoutes.js
@@ -22,6 +22,7 @@ router.route("/signup").post(function (req, res, next) {
     req.logIn(user, function (err) {
       if (err) { return next(err) }
       console.log("successful login")
+      req.session.save({userId:user})
       res.redirect('/');
     });
   })(req, res, next);

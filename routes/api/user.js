2092ec6943a2f876c6d8883a998952797617fef1
H. B. Young <HarryBeckerYoung@gmail.com>
2019-01-12 18:22:57 -0500

    small change in api/user.js

--- routes/api/user.js
+++ routes/api/user.js
@@ -9,17 +9,13 @@ router.route("/create")
 router.route("/find/:id")
 .get(userController.findUser);
 
-<<<<<<< HEAD
+//route for friend requests
 router.route("/friend")
 .get(userController.userByUserName)
 .post(userController.newFriendRequest)
 .put(userController.handleFriendRequest)
-=======
-router.route("/friend/add/:id")
-.post(userController.addFriend);
 
 router.route("/feed/")
 .get(userController.getFeed)
->>>>>>> 4f0bea122bb07738417276ae924482419e99bebe
 
 module.exports = router;
